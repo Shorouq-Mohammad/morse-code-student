3989f65f0050748b2be235f4b3fc5893f11afdf2
Shorouq Fakhoury <Shorouq.fakhoury@gmail.com>
2020-12-17 22:21:08 +0200

    morse exercises

--- MorseCode.js
+++ MorseCode.js
@@ -24,14 +24,52 @@ class ScoreTree extends BSTree {
             this.rightChild = new ScoreTree(key, score)
         }
     }
-    findLetter () {
+    findLetter (score, node = this) {
         // this method should record the path to a given letter
+        if(node.score === score){
+            return ''
+        }else if(score > node.score){
+            return '-' + this.findLetter(score, node.rightChild)
+        }else{
+            return '.' + this.findLetter(score, node.leftChild)
+        }
     }
-    translateWord() {
+    translateWord(word) {
         //this method should translate a given word from text to Morse Code
+        word = word.toUpperCase()
+        let code = ''
+        for(let ch of word){
+            if(ch === ' '){
+                code += '/'
+            }else{
+                code += this.findLetter(alphabet[ch])+ ' '
+            }
+        }
+        console.log(code);
+        return code
     }
-    translateMorse() {
+    translateMorse(morse) {
         // this function should translate a given code from Morse to English
+        let result = ''
+        const characters = morse.split(' ')
+        for(let chara of characters){
+            result += this.findLetterFromMorse(chara)
+        }
+        console.log(result);
+        return result
+    }
+    findLetterFromMorse(chara, node =this){
+        if(chara.length === 0){
+            return node.value
+        }else if(chara.charAt(0) === '.'){
+            chara = chara.substring(1)
+            return this.findLetterFromMorse(chara, node.leftChild)
+        }else if(chara.charAt(0) === '-'){
+            chara = chara.substring(1)
+            return this.findLetterFromMorse(chara, node.rightChild)
+        }else if(chara.charAt(0) === '/'){
+            return ' '
+        }
     }
 }
 //initializing the MorseCode tree
@@ -42,5 +80,5 @@ Object.keys(alphabet).forEach(l => {
 
 morseCode.translateWord("welcome") // should print .-- . .-.. -.-. --- -- . 
 morseCode.translateWord("elevation is cool") // should print . .-.. . ...- .- - .. --- -. /.. ... /-.-. --- --- .-.. 
-morseCode.translateMorse(".... ---- ....")
+morseCode.translateMorse(".... --- ....")
 morseCode.translateMorse("-. .. -.-. . / .--- --- -... / --- -. / - .... . / .-.. . ... ... --- -.")
